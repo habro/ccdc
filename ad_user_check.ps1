4b87cf8e87ba98cc597296f80a90cf06c1409bfe
habro <habrothers12@gmail.com>
2016-04-19 13:59:02 -0500

    Update ad_user_check.ps1

--- ad_user_check.ps1
+++ ad_user_check.ps1
@@ -1,7 +1,8 @@
-$When = ((Get-Date).AddHours(-1)).Date
-$users = Get-ADUser -Filter {whenCreated -ge $When} -Properties whenCreated
+$When = ((Get-Date).AddMinutes(-1)).Date
+$user = Get-ADUser -Filter {whenCreated -ge $When} -Properties whenCreated
 
-if($users){
+if($user){
+	$date = Get-Date
 	$sh = new-object -comobject wscript.shell
-	$msg = $sh.popup(("We found a new user, should we remove " + $users +"?"),0,"YOU CAN WIN!",4)
+	$msg = $sh.popup(("We found a new user " + $user + " at " + $date + "!"),0,"YOU CAN WIN!",0)
 }
